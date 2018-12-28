012e4346b490acfbd4046203d8516b1a195b8dd6
Jon V <jonromero@Jons-MacBook-Pro.local>
2018-12-27 17:45:37 -0800

    image is now flatten but the accurancy is still off

--- generate_shapes.py
+++ generate_shapes.py
@@ -19,26 +19,28 @@ from PIL import Image, ImageDraw
 import numpy as np
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.callbacks import EarlyStopping
 
 SHAPE_SIZE=8
-NUM_OF_SAMPLES=50
+NUM_OF_SAMPLES=50000
 RECTANGLE = 0
 CIRCLE = 1
 
-def create_random_shape(id=None):
+def create_random_shape(id=None, save=False):
     x1, y1, x2, y2 = random.sample(range(0, SHAPE_SIZE), 4)
 
     canvas = Image.new('RGBA', (SHAPE_SIZE, SHAPE_SIZE), "white")
     shape = ImageDraw.Draw(canvas)
-    type_of_shape = RECTANGLE
+    type_of_shape = RECTANGLE # TODO: this need one-hot vector
     shape.rectangle(((x1,y1),(x2,y2)), fill="black")
-    canvas.save("Data/output"+str(id)+".png", "PNG")
+    if save:
+        canvas.save("Data/output"+str(id)+".png", "PNG")
 
     shape_np = np.array(canvas)
-
+    shape_np = shape_np.reshape(SHAPE_SIZE*SHAPE_SIZE*4)
+    
     # return the image in numpy, the bounding box and the type
     return(shape_np, np.array([x1, y1, x2, y2, type_of_shape]))
 
@@ -46,7 +48,7 @@ generated_data = map(create_random_shape, range(NUM_OF_SAMPLES))
 
 # train data X is the image
 # train data y is the validate data: shape and coordinates
-data_input = np.array(map(lambda x: x[1], generated_data))
+data_input = np.array(map(lambda x: x[0], generated_data))
 data_validate = np.array(map(lambda x: x[1:][0], generated_data))
 
 def create_train_set(data):
@@ -64,15 +66,23 @@ train_y, test_y = create_train_set(data_validate)
 model = Sequential([
         Dense(200, input_dim=data_input.shape[-1]), 
         Activation('relu'), 
-        Dropout(0.2), 
+        #Dropout(0.2), 
         Dense(data_validate.shape[-1])
     ])
 
 model.compile(optimizer='adadelta', loss='mse')
 
-# Train.
-model.fit(train_X, train_y, epochs=30, validation_data=(test_X, test_y), verbose=2)
+# Train
+model.fit(train_X, train_y, epochs=100, validation_data=(test_X, test_y), verbose=2)
 
 # save the model
-print("----------Saving the model------------")
-model.save('shape_reko.h5')
+#print("----------Saving the model------------")
+#model.save('shape_reko.h5')
+
+# let's try out the model 
+test_y_predictions = model.predict(test_X)
+print("first element of prediction")
+print(test_y_predictions[0])
+print(test_X[0])
+print(test_y[0])
+
