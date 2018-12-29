fbca282f84899d9d899055210d0a9c8423a112bb
Jon V <jonromero@Jons-MacBook-Pro.local>
2018-12-28 22:55:21 -0800

    experimenting with extraction but I need bigger model

--- extract_shapes.py
+++ extract_shapes.py
@@ -11,11 +11,11 @@ from PIL import Image, ImageDraw, ImageOps
 import numpy as np
 from tensorflow.keras.models import load_model
 
-MAX_SHAPE_SIZE = 32
+MAX_SHAPE_SIZE = 8
 
-filename = "test_extract_data/shape_test.png"
+#filename = "test_extract_data/shape_test.png"
 
-#filename = "test_extract_data/test.png"
+filename = "test_extract_data/test.png"
 
 # convert image file to 1-d numpy array
 # good luck with colors
@@ -27,20 +27,22 @@ desired_size = MAX_SHAPE_SIZE
 ratio = float(desired_size)/max(old_size)
 new_size = tuple([int(x*ratio) for x in old_size])
 
-img = img.resize(new_size)
+img = img.resize(new_size, Image.ANTIALIAS)
 
 delta_w = desired_size - new_size[0]
 delta_h = desired_size - new_size[1]
 padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
-img = ImageOps.expand(img, padding)
+img = ImageOps.expand(img, padding, fill="white")
 
 img.save("test_extract_data/new.png")
 
 img_np = np.array(img).reshape(MAX_SHAPE_SIZE, MAX_SHAPE_SIZE)
-img_np[img_np == 0] = 1
-img_np[img_np > 1] = 0
 img_np = img_np.reshape(1, -1)
+img_np[img_np < 255] = 1
+img_np[img_np == 255] = 0
 
+
+print img_np
 # load the model for shape identification 
 model = load_model('models/shape_reko_'+str(MAX_SHAPE_SIZE)+'x'+str(MAX_SHAPE_SIZE)+'.h5')
 
